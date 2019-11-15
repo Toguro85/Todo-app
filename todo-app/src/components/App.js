11cd55e85ecc051f0a3e5d0eece472e2d39a98b7
Abhinav <abhinavshroff98@gmail.com>
2019-11-15 22:20:22 +0530

    Create basic structure

---
+++ todo-app/src/components/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+function App(){
+    return (
+        <div>
+            <input type="checkbox"/>
+            <p>Placeholder Text</p>
+            
+            <input type="checkbox"/>
+            <p>Placeholder Text</p>
+            
+            <input type="checkbox"/>
+            <p>Placeholder Text</p>
+            
+            <input type="checkbox"/>
+            <p>Placeholder Text</p>
+        </div>
+    )
+}
+
+export default App;
