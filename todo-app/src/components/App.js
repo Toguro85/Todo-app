0faabbf1552c0b60eb718ebac92c89e5a445bcb4
Abhinav <abhinavshroff98@gmail.com>
2019-11-21 20:42:51 +0530

    Added props

--- todo-app/src/components/App.js
+++ todo-app/src/components/App.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import '../styles/App.css';
 import TodoItem from './TodoItem';
+import todosData from './todosData'
 
 function App(){
+    const todo = todosData.map(task => <TodoItem key={task.id} item={task}/>)
     return (
         <div className="todo-list">
-            <TodoItem />
-            <TodoItem />
-            <TodoItem />
-            <TodoItem />
+            {todo}
         </div>
     )
 }
