68c613f641e6f509f79eba47d2a91dcd81e480e0
Abhinav <abhinavshroff98@gmail.com>
2019-11-23 13:49:15 +0530

    Conditional Rendering

--- todo-app/src/components/TodoItem.js
+++ todo-app/src/components/TodoItem.js
@@ -2,7 +2,13 @@ import React from 'react';
 import '../styles/TodoItem.css'
 
 class TodoItem extends React.Component{
+    
     render(){
+        const styles = {
+            fontStyle : "italic",
+            color : "#cdcdcd",
+            textDecoration : "line-through"
+        }
         return(
         <div className="todo-item"> 
         <input 
@@ -10,7 +16,7 @@ class TodoItem extends React.Component{
         checked={this.props.item.completed}
         onChange = {() => this.props.handleClick(this.props.item.id)}
         />
-        <p>{this.props.item.text}</p>
+        <p style={this.props.item.completed ? styles : null}>{this.props.item.text}</p>
         </div>
         )
     }
